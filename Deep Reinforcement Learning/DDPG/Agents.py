e0bdda8acb35f4e69e1941114c720dffcd9d69bd
Tanmay Pathrabe <64797614+M-NEXT@users.noreply.github.com>
2021-08-16 21:08:08 +0530

    Add files via upload
    New agent file added

---
+++ Deep Reinforcement Learning/DDPG/Agents.py
@@ -0,0 +1,117 @@
+import ReplayBuffer as rb
+import Networks as networks
+import Noise
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+class Agent():
+    def __init__(self, input_dims, num_actions, lr_actor, lr_critic, gamma, tau, batch_size, capacity, device):
+        self.num_actions = num_actions
+        self.input_dims = input_dims
+        self.lr_actor = lr_actor
+        self.lr_critic = lr_critic
+        self.gamma = gamma
+        self.tau = tau
+        #self.device = device
+        self.memory = rb.ReplayMemory(capacity, num_actions, batch_size, input_dims)
+        self.critic = networks.Critic(input_dims, num_actions, lr_critic).to(device)
+        self.critic_target = networks.Critic(input_dims, num_actions, lr_critic).to(device)
+        self.actor = networks.Actor(input_dims, num_actions, lr_actor).to(device)
+        self.actor_target = networks.Actor(input_dims, num_actions, lr_actor).to(device)
+        self.noise = Noise.OUActionNoise(mu=np.zeros(num_actions))#.to(device)
+        
+
+    def select_action(self, obs, device):
+        self.actor.eval()
+        obs = torch.tensor([obs], dtype=torch.float32).to(device)
+        mu = self.actor.forward(obs)
+        mu = mu + torch.tensor(self.noise(), dtype=torch.float32).to(device)
+        mu = mu.cpu().detach().numpy()[0]#reshape()
+        self.actor.train()
+        return mu
+
+    def get_tensors(self, batch, device):
+        states = torch.tensor(batch[0]).to(device)
+        actions = torch.tensor(batch[1], dtype=torch.float32).to(device)
+        rewards = torch.tensor(batch[2]).to(device)
+        next_states = torch.tensor(batch[3]).to(device)
+        dones = torch.tensor(batch[4]).to(device)
+        #print(type(actions))
+        return states, actions, rewards, next_states, dones
+
+
+    def learn(self, device):
+        if self.memory.can_provide_batch():
+            batch = self.memory.give_batch()
+            states, actions, rewards, next_states, dones = self.get_tensors(batch, device)
+            #print(type(actions))
+            next_actions = self.actor_target.forward(next_states)
+            next_q_value = self.critic_target.forward(next_states, next_actions)
+            current_q_value = self.critic.forward(states, actions)
+
+            # self.critic.eval()
+            # self.critic_target.eval()
+            # self.actor_target.eval()
+
+            next_q_value[dones] = 0
+            #next_q_value = next_q_value.reshape(-1,1)
+            #rewards = rewards.reshape(-1,1)
+            #dones = dones.reshape(-1,1)
+            #print(next_q_value.shape, dones.shape)
+            #next_q_value = next_q_value * dones
+            #print(next_q_value.shape)
+            next_q_value = next_q_value.view(-1)
+            #current_q_value.reshape(-1,1)
+            calc_q_value = rewards + self.gamma * next_q_value # * dones
+            calc_q_value = calc_q_value.reshape(-1,1)#.detach()
+            #print(next_q_value.shape, rewards.shape, calc_q_value.shape)
+
+            #self.critic.train()
+            self.critic.optimizer.zero_grad()
+            critic_loss = F.mse_loss(calc_q_value, current_q_value) #self.critic.loss(calc_q_value, current_q_value)
+            critic_loss.backward()
+            self.critic.optimizer.step()
+
+            self.critic.eval()
+            self.actor.optimizer.zero_grad()
+            #self.critic.eval()
+            #self.actor.eval()
+            temp_actions = self.actor.forward(states)
+            #self.actor.train()
+            actor_loss = -self.critic.forward(states, temp_actions)
+            actor_loss = torch.mean(actor_loss)
+            actor_loss.backward()
+            self.actor.optimizer.step()
+            self.critic.train()
+
+            self.update_target_networks(self.tau)
+
+    def update_target_networks(self, tau):
+        actor_params = dict(self.actor.named_parameters())
+        actor_target_params = dict(self.actor_target.named_parameters())
+        critic_params = dict(self.critic.named_parameters())
+        critic_target_params = dict(self.critic_target.named_parameters())
+
+        for name in actor_params:
+            actor_params[name] = tau*actor_params[name].clone() + (1-tau)*actor_target_params[name].clone()
+
+        for name in critic_params:
+            critic_params[name] = tau*critic_params[name].clone() + (1-tau)*critic_target_params[name].clone()
+
+        self.actor_target.load_state_dict(actor_params)
+        self.critic_target.load_state_dict(critic_params)
+        
+    def save_model(self, chkpt):
+    	print('--------Saving Model--------')
+    	torch.save(self.actor.state_dict(), chkpt)
+    	torch.save(self.actor_target.state_dict(), chkpt)
+    	torch.save(self.critic.state_dict(), chkpt)
+    	torch.save(self.critic_target.state_dict(), chkpt)
+    	
+    def load_model(self, chkpt):
+    	print('--------Loading Model--------')
+    	self.actor.load_state_dict(torch.load(chkpt))
+    	self.actor_target.load_state_dict(torch.load(chkpt))
+    	self.critic.load_state_dict(torch.load(chkpt))
+    	self.critic_target.load_state_dict(torch.load(chkpt))
